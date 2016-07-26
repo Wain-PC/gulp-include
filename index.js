111cae0e9df26730cd9dac5aeb3722dde133c79a
Wain-PC <wain-pc@mail.ru>
2016-07-26 14:25:39 +0300

    -Added support for @include("path/to/file.js") or @require("path/to/file.js") -Added support for conditional replacements (.not("condition") and .is("condition")) -Minor code improvements

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var fs = require('fs'),
     gutil = require('gulp-util'),
     glob = require('glob'),
     applySourceMap = require('vinyl-sourcemaps-apply'),
-    stripBom = require('strip-bom');
+    stripBom = require('strip-bom'),
+    argv = require('yargs').argv;
 
 var SourceMapGenerator = require('source-map').SourceMapGenerator;
 var SourceMapConsumer = require('source-map').SourceMapConsumer;
@@ -76,10 +77,12 @@ module.exports = function (params) {
 };
 
 function processInclude(content, filePath, sourceMap) {
-    var matches = content.match(/^(\s+)?(\/\/|\/\*|\#|\<\!\-\-)(\s+)?=(\s+)?(include|require)(.+$)/mg);
+    var re = /\/\/\s*?@(\w+)\("([^"]+)"\)(?:\.(is|not)\(\"([^\"]+)\"\))*\s*;?/gm;
+    var match = re.exec(content),
+        firstIteration = true;
     var relativeBasePath = path.dirname(filePath);
 
-    if (!matches) return {content: content, map: null};
+    if (!match) return {content: content, map: null};
 
     // Apply sourcemaps
     var map = null, mapSelf, lastMappedLine, currentPos, insertedLines;
@@ -110,27 +113,59 @@ function processInclude(content, filePath, sourceMap) {
         };
     }
 
-    for (var i = 0; i < matches.length; i++) {
-        var leadingWhitespaceMatch = matches[i].match(/^\s*/);
+    do
+    {
+        //get the next match
+        if (!firstIteration) {
+            match = re.exec(content);
+        }
+        else {
+            firstIteration = false;
+        }
+        if (!match) {
+            break;
+        }
+        //dunno what's that for, so let's just put a dummy here.
+        var leadingWhitespaceMatch = [' '];
         var leadingWhitespace = null;
         if (leadingWhitespaceMatch) {
             leadingWhitespace = leadingWhitespaceMatch[0].replace("\n", "");
         }
 
         // Remove beginnings, endings and trim.
-        var includeCommand = matches[i]
-            .replace(/\s+/g, " ")
-            .replace(/(\/\/|\/\*|\#|<!--)(\s+)?=(\s+)?/g, "")
-            .replace(/(\*\/|-->)$/g, "")
-            .replace(/['"]/g, "")
-            .trim();
+        var includeType = match[1],
+            filePath = match[2]
+                .replace(/\s+/g, " ")
+                .replace(/(\/\/|\/\*|\#|<!--)(\s+)?=(\s+)?/g, "")
+                .replace(/(\*\/|-->)$/g, "")
+                .replace(/['"]/g, "")
+                .trim(),
+            includeCondition = match[3],
+            includePropName = match[4],
+            currentLine;
 
-        var split = includeCommand.split(" ");
+        //Decide whether to process current include or not, depending on the params provided
+        if (includeCondition && includePropName) {
+            //if the condition is 'is' and property name matches, proceed
+            //also proceed when the condition is 'not' and property name does NOT match
+            if (!((includeCondition === 'is' && argv.hasOwnProperty(includePropName)) ||
+                (includeCondition === 'not' && !argv.hasOwnProperty(includePropName)))) {
+                //console.log("Skipping %s (%s %s)", match[0], includeCondition, includePropName);
+                //As we skip the file, we should remove the comment
+                content = content.replace(match[0], '');
+                re.lastIndex -= match[0].length;
+                continue;
+            }
+        }
 
-        var currentLine;
+        if (includeCondition === 'is') {
+            debugger;
+        }
+
+
         if (sourceMap) {
             // get position of current match and get current line number
-            currentPos = content.indexOf(matches[i], currentPos);
+            currentPos = content.indexOf(match[0], currentPos);
             currentLine = currentPos === -1 ? 0 : content.substr(0, currentPos).match(/^/mg).length;
 
             // sometimes the line matches the leading \n and sometimes it doesn't. wierd.
@@ -140,33 +175,31 @@ function processInclude(content, filePath, sourceMap) {
             mapSelf(currentLine);
         }
 
-        // SEARCHING STARTS HERE
-        // Split the directive and the path
-        var includeType = split[0];
-
         // Use glob for file searching
         var fileMatches = [];
-        var includePath = "";
+        var includePath = "",
+            y, globResults;
 
         if (includePaths != false) {
             // If includepaths are set, search in those folders
-            for (var y = 0; y < includePaths.length; y++) {
-                includePath = includePaths[y] + "/" + split[1];
+            for (y = 0; y < includePaths.length; y++) {
+                includePath = includePaths[y] + "/" + filePath;
 
-                var globResults = glob.sync(includePath, {mark: true});
+                globResults = glob.sync(includePath, {mark: true});
                 fileMatches = fileMatches.concat(globResults);
             }
-        } else {
+        }
+        else {
             // Otherwise search relatively
-            includePath = relativeBasePath + "/" + split[1];
-            var globResults = glob.sync(includePath, {mark: true});
+            includePath = relativeBasePath + "/" + filePath;
+            globResults = glob.sync(includePath, {mark: true});
             fileMatches = globResults;
         }
 
         if (fileMatches.length < 1) fileNotFoundError(includePath);
 
         var replaceContent = '';
-        for (var y = 0; y < fileMatches.length; y++) {
+        for (y = 0; y < fileMatches.length; y++) {
             var globbedFilePath = fileMatches[y];
 
             // If directive is of type "require" and file already included, skip to next.
@@ -179,6 +212,7 @@ function processInclude(content, filePath, sourceMap) {
             // Unicode byte order marks are stripped from the start of included files
             var fileContents = stripBom(fs.readFileSync(globbedFilePath));
 
+            //console.log("Including %s", match[0]);
             var result = processInclude(fileContents.toString(), globbedFilePath, sourceMap);
             var resultContent = result.content;
 
@@ -263,10 +297,16 @@ function processInclude(content, filePath, sourceMap) {
                 replaceContent = '\n' + replaceContent;
             }
 
-            content = content.replace(matches[i], function() { return replaceContent });
+            content = content.replace(match[0], function () {
+                return replaceContent;
+            });
             insertedLines--; // adjust because the original line with comment was removed
         }
-    }
+        else {
+            //condition didn't work out, but we should remove the comment anyway
+            content = content.replace(match[0], '');
+        }
+    } while (match);
 
     if (sourceMap) {
         currentLine = content.match(/^/mg).length + 1;
